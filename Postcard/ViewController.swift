011abdb273e96aa83c77e025db13012662ca9053
Clint Chang <clintchang@gmail.com>
2014-11-06 00:33:51 -0500

    added a comment to test commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // This is a comment here to test commits 
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         
